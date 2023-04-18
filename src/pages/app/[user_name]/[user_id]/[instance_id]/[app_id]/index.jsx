30c2378a17ba1b70f5b0a900f113df7e6e8f4ce6
Javier Monterroso <jvmonteros98@gmail.com>
2023-04-18 08:49:38 -0600

    hero image

--- src/pages/app/[user_name]/[user_id]/[instance_id]/[app_id]/index.jsx
+++ src/pages/app/[user_name]/[user_id]/[instance_id]/[app_id]/index.jsx
@@ -13,6 +13,7 @@ import { getTokensByAppId } from "src/services/auth.js";
 
 // components
 import { Cards } from "../../../../../../components/dashboard/Cards"
+import { Hero } from 'src/components/dashboard/Hero'
 
 export default function Configuration() {
   const router = useRouter()
@@ -93,9 +94,7 @@ export default function Configuration() {
       {/* Features */}
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <center>
-            <img className="lg:w-1/6 md:w-2/6 w-4/6 mb-10 object-cover object-center rounded" alt="hero" src="//www.dxlatam.com/wp-content/uploads/2021/04/New-Project-2.png" />
-          </center>
+          <Hero />
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900">Funcionalidades</h1>
           <h2 className="sm:text-2xl text-xl title-font text-center text-gray-700 font-light mb-20">Administra tu cuenta publicitaria desde un solo lugar</h2>
           <Cards />
